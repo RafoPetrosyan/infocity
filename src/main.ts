27e08f3eaec76856cea295959908e22a71b5dc3a
Rafo Petrosyan <petrosyanrafo0@gmail.com>
2025-07-11 11:44:14 +0400

    changes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+  });
+
   const i18n = app.get(I18nService);
   app.useGlobalPipes(
     new ValidationPipe({
